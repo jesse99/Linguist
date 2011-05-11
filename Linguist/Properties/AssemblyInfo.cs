3796584923684a313856c2433ef1ea88447db6d7
Jesse Jones <jjones@cococorp.com>
2011-05-10 19:28:59 -0700

    Updated description and version

--- Linguist/Properties/AssemblyInfo.cs
+++ Linguist/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Linguist")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Adds customizable syntax highlighting.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Linguist")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
